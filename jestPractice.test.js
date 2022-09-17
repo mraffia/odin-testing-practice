41120373c17ca76a26759f5bf88d45981f586377
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-09-18 00:20:45 +0700

    Add more tests for capitalize function

--- jestPractice.test.js
+++ jestPractice.test.js
@@ -1,3 +1,15 @@
-test("capitalize('bruh') to return Bruh", () => {
+test("capitalize('bruh') to return 'Bruh'", () => {
     expect(capitalize('bruh')).toMatch(/Bruh/);
+});
+
+test("capitalize('Bruh') to return 'Bruh'", () => {
+    expect(capitalize('Bruh')).toMatch(/Bruh/);
+});
+
+test("capitalize('') to return blank/empty space", () => {
+    expect(capitalize('')).toMatch(/\s/);
+});
+
+test("capitalize() to return 'You didn't provide a word.'", () => {
+    expect(capitalize()).toMatch(/You didn't provide a word./);
 });
