4016f8de51d9f13f69190e9f1cdeca855e70c649
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-09-18 13:34:35 +0700

    Add calculator object tests

--- jestPractice.test.js
+++ jestPractice.test.js
@@ -1,5 +1,6 @@
 import { capitalize, reverseString } from "./jestPractice.js"
 
+// capitalize
 test("capitalize('bruh') to return 'Bruh'", () => {
     expect(capitalize('bruh')).toMatch(/Bruh/);
 });
@@ -12,7 +13,7 @@ test("capitalize('') to return blank", () => {
     expect(capitalize('')).toMatch(/\s*/);
 });
 
-
+// reverseString
 test("reverseString('Bruh') to return 'hurB'", () => {
     expect(reverseString('Bruh')).toMatch(/hurB/);
 });
@@ -23,4 +24,21 @@ test("reverseString('B') to return 'B'", () => {
 
 test("reverseString('') to return blank", () => {
     expect(reverseString('')).toMatch(/\s*/);
+});
+
+// calculator
+test("calculator.add(1, 1) to return 2", () => {
+    expect(reverseString(1, 1)).toBe(2);
+});
+
+test("calculator.subtract(1, 1) to return 0", () => {
+    expect(reverseString(1, 1)).toBe(0);
+});
+
+test("calculator.divide(20, 4) to return 5", () => {
+    expect(reverseString(20, 4)).toBe(5);
+});
+
+test("calculator.multiply(20, 4) to return 80", () => {
+    expect(reverseString(20 ,4)).toBe(80);
 });
